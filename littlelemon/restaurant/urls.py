4d47007c475ad5e77247fdc6b2f079fa0a80cbdc
Thabo <thabo@macbook-pro.lan>
2024-03-22 14:22:21 -0600

    addition of serializers and updates to models

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # Define the URL pattern for the index view
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,28 @@
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
